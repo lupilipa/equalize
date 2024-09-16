2df89f85435583d738611f7fa7d73b46f6716585
Luana Moreira <luanamoreirafm@gmail.com>
2024-09-16 00:39:22 -0300

    feat: padrao de nav e footer adm

--- views/pages/admin/relatos_adm.php
+++ views/pages/admin/relatos_adm.php
@@ -7,22 +7,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relatos - Equalize</title>
     <link rel="icon" href="../../media/logo.png" type="image/png">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../../css/style.css">
 
 </head>
 
 <body>
 
-    <nav>
-        <a class="btn-sair" href="../../../logica/controllers/ControllerLogin.php?logout">Sair</a></br>
-        <a class="btn-menu" href="home_adm.php">Home</a></br>
-        <a class="btn-menu" href="ajuda_adm.php">Ajuda: ADM</a></br>
-        <a class="btn-menu" href="relatos_adm.php">Relatos: ADM</a></br>
-    </nav>
+    <header>
+        <input type="checkbox" name="menuHamb" id="menuHambId" style="display:none;">
+        <div class="container">
+            <label for="menuHambId"><div class="hamburger"></div></label>
+        </div>
+        <nav id="menu-links">
+            <ul>
+                <li><a class="btn-sair" href="../../../logica/controllers/ControllerLogin.php?logout">Sair</a></li>
+                <li><a class="btn-menu" href="home_adm.php">Home</a></li>
+                <li><a class="btn-menu" href="ajuda_adm.php">Ajuda: ADM</a></li>
+                <li><a class="btn-menu" href="relatos_adm.php">Relatos: ADM</a></li>
+            </ul>
+        </nav>
+        <div class="fade"></div>
+        <div class="titulo">
+            <h1>Equalize</h1>
+            <h2>Home</h2>
+        </div>
+    </header>
     
     <section>
         <h1>Relatos: ADM</h1>
     </section>
+
+    <footer>
+        <div class="copyright">
+            <p>&copy; Desenvolvido por Luana e Ramon.</p></div>
+        <div class="escola">
+            <p>E.E.E.P Salaberga Torquarto Gomes de Matos</p></div>
+        <div class="links">
+            <div class="paginas">
+                <ul>
+                    <li><a class="btn-menu" href="home_adm.php">Home</a></li>
+                    <li><a class="btn-menu" href="ajuda_adm.php">Ajuda: ADM</a></li>
+                    <li><a class="btn-menu" href="relatos_adm.php">Relatos: ADM</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+    
+    <script src="../../js/fade.js"></script>
 
 </body>
 
