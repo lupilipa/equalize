d16171e26124657e52db7fc6ac6bb6e292828a3d
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-09-28 19:18:25 -0300

    footer + css

--- views/pages/autenticacao/error_login.php
+++ views/pages/autenticacao/error_login.php
@@ -7,16 +7,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Erro - Equalize</title>
     <link rel="icon" href="../../media/logos.png" type="image/png">
+    <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/login.css">
 
 </head>
 
 <body>
 
-    <section>
+    <main>
         <h1>Erro no login</h1>
         <a class="btn-menu" href="login.php">Tente de novo</a></br>
-    </section>
+    </main>
+    <footer>
+        <div class="copyright">
+            <p>&copy; Desenvolvido por Luana e Ramon.</p></div>
+        <div class="escola">
+            <p>E.E.E.P Salaberga Torquarto Gomes de Matos</p></div>
+        <div class="links">
+            <div class="paginas">
+                <ul>
+                    <li><a class="btn-menu" href="identificar/como_identificar.php">Como identificar?</a></li>
+                    <li><a class="btn-menu" href="pedir_ajuda/pedir_ajuda.php">Pedir Ajuda</a></li>
+                    <li><a class="btn-menu" href="relatos_ver.php">Relatos</a></li>
+                    <li><a class="btn-menu" href="projeto.php">O projeto</a></li>
+                    <li><a class="btn-menu" href="contatos.php">Contatos</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 
 </body>
 
