96b8d31a8d31eac3ab0c544a48f6d240c3968aef
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-09-28 22:52:50 -0300

    css

--- views/pages/anonimo/home_anonimo.php
+++ views/pages/anonimo/home_anonimo.php
@@ -9,6 +9,7 @@
     <link rel="icon" href="../../media/logos.png" type="image/png">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/index.css">
 
 </head>
 
